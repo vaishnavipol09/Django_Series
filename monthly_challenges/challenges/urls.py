4e93a1693564c2dec1c571b08e0f3a752fec4ed1
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-11 15:52:42 +0530

    convert month string into integer value

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path("january" , views.index)
+    path("<int:month>" , views.monthly_challenges_by_integer),
+    path("<str:month>" , views.monthly_challenges)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render # type: ignore
-from django.http import HttpResponse # type: ignore
+from django.http import HttpResponse , HttpResponseNotFound # type: ignore
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Welcome to the monthly challenges app!")
+def monthly_challenges_by_integer(request , month):
+    return HttpResponse(month)
+
+def monthly_challenges(request , month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Welcome to January month"
+    elif month == "february":
+        challenge_text = "Welcome to February month"    
+    elif month == "march":
+        challenge_text = "Welcome to march month"    
+    elif month == "april":
+        challenge_text = "Welcome to april month"    
+    elif month == "may":
+        challenge_text = "Welcome to may month" 
+    else:
+        return HttpResponseNotFound("Month not found")    
+
+    return HttpResponse(challenge_text)           
 
 
